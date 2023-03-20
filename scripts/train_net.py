e6d281cdf8a44e18e871c221ba96d2ca3ea88a5a
Cesare Tonola <c.tonola001@unibs.it>
2023-03-20 15:33:55 +0100

    python train script

--- scripts/train_net.py
+++ scripts/train_net.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
+from datetime import datetime
+from torch.utils.data.dataloader import default_collate
 
 # Params
 dof = 6
@@ -11,13 +13,15 @@ load_net = False
 max_scaling = 1000
 nn_name = "nn_ssm.pt"
 dataset_name = "ssm_dataset.bin"
+batch_size = 32
+n_epochs = 1000
 
 # Get paths
 PATH = os.path.dirname(os.path.abspath(__file__)) + "/scripts/data/"
 dataset_path = PATH + dataset_name
 nn_path = PATH + nn_name
 
-# Get cpu or gpu device for training.
+# Get cpu or gpu device for training
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
@@ -25,93 +29,95 @@ print(f"Using {device} device")
 raw_data = np.fromfile(dataset_path, dtype='float') # array containing the data
 length = raw_data.size
 
-cols = dof + dof + 3 + 1  # q, dq, (x,y,z) of obstacle, scaling
+cols = dof + dof + 3 + 1  # q, dq, (x,y,z) of obstacle, scalings
 rows = int(length/cols)
 
 raw_data = raw_data.reshape(rows,cols) # from array to multi-dimensional-array
 
-# Set max scaling value 
-scaling = raw_data[:,cols] # last column
-scaling = np.where[scaling>max_scaling,max_scaling,scaling]
+# Set max scalings value 
+scalings = raw_data[:,-1] # last column
+scalings = np.where[scalings>max_scaling,max_scaling,scalings]
 
 # Define inputs and outputs
-input  = torch.Tensor(raw_data[:,0:cols-1])  # q, dq, (x,y,z) of obstacle
-output = torch.Tensor(scaling)
+input  = torch.Tensor(raw_data[:,0:-1])  # q, dq, (x,y,z) of obstacle (last column excluded)
+output = torch.Tensor(scalings)
 
-print(f"First two samples {input[0:1,:]}")
+print(f"First two samples {input[0:2,:]}")
 
-# TODO: fai dataset di train e di test
-
+# Create trainingg and validation datasets
 dataset = torch.utils.data.TensorDataset(input,output) # create your datset
-dataloader = torch.utils.data.DataLoader(dataset,100) # create your dataloader
-dataloader_plot = torch.utils.data.DataLoader(dataset,100000) # create your dataloader
+dataset = dataset.shuffle(buffer_size=rows,seed=datetime.now())
+
+train_size = int(rows*0.8)
+test_size = int(rows*0.2)
+
+train_dataset = dataset.take(train_size).to(device)    
+test_dataset = dataset.skip(train_size).take(test_size).to(device) 
+
+train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: default_collate(x).to(device))
+test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True, collate_fn=lambda x: default_collate(x).to(device))    
 
 if load_net:
-   net=torch.load(nn_path)
+   NN=torch.load(nn_path)
 else:
-   net = nn.Sequential(
-         nn.Linear(dof+3, 500),
+   NN = nn.Sequential(
+         nn.Linear(dof+dof+3, 100),
          nn.ReLU(),
-         nn.Linear(500, 100),
-         nn.ReLU(),
-         nn.Linear(100, 50),
-         nn.ReLU(),
-         nn.Linear(50, 10),
-         nn.ReLU(),
-         nn.Linear(10, 1)
-         ).to(device)
+         nn.Linear(100, 1),
+         nn.ReLU()).to(device)
 
+print(NN)
+print(NN(input[1,:]))
 
-# dropout
+# Define loss function and optimizers
 
-print(net)
-print(net(input[1,:]))
+#criterion = torch.nn.L1Loss()
+criterion = torch.nn.MSELoss()
 
-criterion = torch.nn.L1Loss()
 #optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+optimizer = optim.Adam(NN.parameters(), lr=0.001)
 
-ordine=2.0
+# Train
+train_loss = []
+test_loss = []
 
-for epoch in range(5000):  # loop over the dataset multiple times
-
+for epoch in range(n_epochs):
+   NN.train() # set training mode
    running_loss = 0.0
-   for i, data in enumerate(dataloader, 0):
-       # get the inputs; data is a list of [inputs, labels]
-       inputs, distance = data
-
-       # zero the parameter gradients
-       optimizer.zero_grad()
+   for i, data in enumerate(train_dataloader, 0):  #batches
+       inputs, scaling = data.to(device) #get the inputs; data is a list of [inputs, labels]
 
        # forward + backward + optimize
-       outputs = net(inputs)
-#       loss = criterion(100*outputs/(distance+1), 100*distance/(distance+1))
-       loss = criterion(0.1*outputs,0.1*distance)
+       optimizer.zero_grad() # zero the parameter gradients
+       outputs = NN(inputs)
+       loss = criterion(outputs,scaling)
        loss.backward()
        optimizer.step()
 
        # print statistics
+       train_loss.append(loss.item())
        running_loss += loss.item()
-       if i % 2000 == 1999:    # print every 2000 mini-batches
+       if i % 1000 == 999:    # print every 2000 mini-batches
            print('[%d, %5d] loss: %.3f' %
-                 (epoch + 1, i + 1, running_loss / 2000))
+                 (epoch + 1, i + 1, running_loss / 1000))
            running_loss = 0.0
 
+   if epoch % 10 == 9:  # print every 10 epoches
+        with torch.no_grad:
+            NN.eval() # set test mode
+            for i, data in enumerate(test_dataloader, 0):
+                inputs, scaling = data.to(device)
+                test_output=NN(inputs)
+                loss = criterion(test_output,scaling)
+                test_loss.append(loss.item())
+                
+            plt.figure(figsize=(10,5))
+            plt.title("Training and Test Loss")
+            plt.plot(test_loss,label="val")
+            plt.plot(train_loss,label="train")
+            plt.xlabel("iterations")
+            plt.ylabel("Loss")
+            plt.legend()
+            plt.show()
 
-   if epoch % 10 == 9:    # print every 2000 mini-batches
-       for i, data in enumerate(dataloader_plot, 0):
-           inputs, distance = data
-           model_output=net(inputs)
-
-           fig, axs = plt.subplots(2)
-           fig.set_size_inches(10,10)
-
-           axs[0].plot(distance.numpy(),model_output.detach().numpy(),'.')
-           axs[0].set(xlabel="distance",ylabel="estimated distance",title="dof"+str(dof))
-           axs[0].grid(True)
-           axs[1].plot(distance.numpy(),model_output.detach().numpy()-distance.numpy(),'.')
-           axs[1].set(xlabel="distance",ylabel="model-real",title="dof"+str(dof))
-           axs[1].grid(True)
-           #fig.savefig("dof3.png",dpi=300)
-       plt.show()
-       torch.save(net, nn_path)
+        torch.save(NN, nn_path)
